e35bbe358ddb3be37bce8eaf1a6673edbc7d04dd
tfuquirk <timothyfquirk@gmail.com>
2018-12-11 10:56:49 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,10 @@
+### 3. Define a class of `Person` in `lib/person.rb`
+
+class Person
+
+end
+
+### 4. Make 2 people with the names of `adele_goldberg` and `alan_kay`
+
+adele_goldberg = Person.new
+alan_kay = Person.new
